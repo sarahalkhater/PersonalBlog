6cefe74f3d59efce1e8aa92bf980fc33e91455aa
Sarah Alkhater <92782796+sarahalkhater@users.noreply.github.com>
2022-05-07 01:17:00 +0300

    MongoDb Added and Changed Files

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const app = new express();
+
+app.use(express.static('public')); //kullanicinin erisebilecegi klasoru belirttik
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://sarahalkhater:Sa.123456@cluster0.trtmf.mongodb.net/personalBlog?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("personalBlog").collection("person");
+    // perform actions on the collection object
+    console.log(err); //eger database e baglanti hatasi olursa console a yazmasi icin.
+    client.close();
+});
+
+
+app.listen(8080, () => {
+    console.log('App listening on port 8080')
+});
