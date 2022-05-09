a17637aace4a98bb0fbb0c3bdfbf27ff0714f84d
Sarah Alkhater <92782796+sarahalkhater@users.noreply.github.com>
2022-05-10 00:44:01 +0300

    about and contact pages added

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'pages/index.html'));
 });
 
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+app.get('/contact.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://sarahalkhater:Sa.123456@cluster0.trtmf.mongodb.net/personalBlog?retryWrites=true&w=majority";
